46f36a42c3395b87d015ee9e423b77575d7706f0
Veselin <veselin.lk.petrov@gmail.com>
2019-04-21 20:57:37 +0300

    added a link to the github repo

--- src/components/AppContent/index.jsx
+++ src/components/AppContent/index.jsx
@@ -6,11 +6,25 @@ import Section3 from '../Section3'
 
 export default function AppContent() {
     return (
-        <div className="app-content section">
-            <h5>App content</h5>
-            <Section1 />
-            <Section2 />
-            <Section3 />
-        </div>
+        <>
+            <div className="app-content section">
+                <h5>App content</h5>
+                <Section1 />
+                <Section2 />
+                <Section3 />
+            </div>
+
+            <p className="github">
+                <a href="https://github.com/atorov/react-hooks-p5js">
+                    <img
+                        alt="github logo"
+                        src="/img/github-logo.png"
+                        border="0"
+                        width="auto"
+                        height="28px"
+                    />
+                </a>
+            </p>
+        </>
     )
 }
